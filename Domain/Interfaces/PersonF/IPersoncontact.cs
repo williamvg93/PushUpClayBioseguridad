b96d423f16ab52db67d12b9ef87fb763aa5c6904
williamvg93 <sonico_5071@hotmail.com>
2023-12-11 10:15:57 -0500

    feat: :sparkles: Added all interfaces for each entity

---
+++ Domain/Interfaces/PersonF/IPersoncontact.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces.PersonF;
+
+public interface IPersoncontact : IGenericRepository<Personcontact>
+{
+
+}
